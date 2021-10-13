05f01c8417d3ffb8f0a31626816981810c07724a
idlopesdev54 <idlopes54@gmail.com>
2021-10-13 11:02:08 -0300

    para estudo

---
+++ sintaxe-variaveis-e-fluxo/src/testaCondicional.java
@@ -0,0 +1,24 @@
+
+public class testaCondicional {
+
+	public static void main(String[] args) {
+		System.out.println("testando condicionais");
+		int idade = 16;
+		int quantidadePessoas = 1;
+		
+		if(idade >= 18 ) {
+			System.out.println("Você tem mais de 18 anos");
+			System.out.println("Seja bem vindo");
+		}else {
+			if(quantidadePessoas >=2) {
+				System.out.println("voce pode entrar, pois esta acompanhado.");
+				} else {
+			
+			System.out.println("infelizmente você nao pode entrar!");
+			System.out.println("Vai embora.");
+			}
+		}
+		
+	}
+}
+
